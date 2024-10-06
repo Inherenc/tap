7f34a8bf10784bafc7f00bb5e6f30ae40aecc822
dv <pavan.divi@outlook.com>
2024-10-06 22:29:00 +0530

    fix: install failed due to integrity check

--- Formula/dtf.rb
+++ Formula/dtf.rb
@@ -5,6 +5,8 @@ class Dtf < Formula
 
     url "https://github.com/pavandv/dtf-manager/releases/download/#{version}/dtf"
 
+    sha256 "b8e308002f7449750ca1b924d88575d552b7c733e5a955ad1f07a736f6d725a5"
+
     def install
         binary_name="dtf"
         bin.install "#{binary_name}"
